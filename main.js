bb6c5ed991d3e0ad031d9c36b52b47a34a87e382
Valuriel <christian.poetter@hotmail.de>
2021-09-06 20:42:19 +0300

    implemented proof-of-work

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp +JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp +JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -29,7 +40,7 @@ class BlockChain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -52,15 +63,9 @@ class BlockChain{
 }
 
 let blockCoin =new BlockChain();
+
+console.log('Mining block 1...');
 blockCoin.addBlock(new Block(1, "06/08/2021", { amount: 4 }));
+
+console.log('Mining block 2...');
 blockCoin.addBlock(new Block(2, "10/08/2021", { amount: 10 }));
-
-console.log('Is blockchain valid? ' + blockCoin.isChainValid());
-
-blockCoin.chain[1].data = { amount: 100 };
-blockCoin.chain[1].hash = blockCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + blockCoin.isChainValid());
-
-
-// console.log(JSON.stringify(blockCoin, null, 4));
